2270a1f1ec0857f5b437f30dfd4d6119e4cab2af
pawelitex3 <38460805+pawelitex3@users.noreply.github.com>
2020-02-21 17:40:33 +0100

    Added TokenService

---
+++ Scheduledo.Service.app.Service/Abstract/ITokenService.cs
@@ -0,0 +1,13 @@
+﻿using Scheduledo.Service.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scheduledo.Service.Abstract
+{
+	public interface ITokenService
+	{
+		Task<Result<string>> GetEmailFromToken(string token);
+	}
+}
---
+++ Scheduledo.Service.app.Service/Concrete/TokenService.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json.Linq;
+using Scheduledo.Service.Abstract;
+using Scheduledo.Service.Models;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scheduledo.Service.Concrete
+{
+	public class TokenService : ITokenService
+	{
+		public async Task<Result<string>> GetEmailFromToken(string token)
+		{
+			var result = new Result<string>();
+			
+			token = token.Remove(0, 7);
+
+			var handler = new JwtSecurityTokenHandler();
+			var tokenS = handler.ReadToken(token) as JwtSecurityToken;
+			JObject jsonPayload = JObject.Parse(tokenS.Payload.SerializeToJson());
+
+			result.Data = jsonPayload.GetValue("http://schemas.xmlsoap.org/ws/2005/05/identity/claims/emailaddress").ToString();
+
+			return result;
+		}
+	}
+}
