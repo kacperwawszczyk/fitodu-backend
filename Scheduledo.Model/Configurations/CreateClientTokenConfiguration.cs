79eedd50dfd71b73bf8e97b62876988142ea9ffc
pawelitex3 <38460805+pawelitex3@users.noreply.github.com>
2020-02-22 23:15:25 +0100

    Added table for CreateClientTokens

---
+++ Scheduledo.Model/Configurations/CreateClientTokenConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Scheduledo.Model.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Scheduledo.Model.Configurations
+{
+    public class CreateClientTokenConfiguration : IEntityTypeConfiguration<CreateClientToken>
+    {
+        public void Configure(EntityTypeBuilder<CreateClientToken> builder)
+        {
+            builder.HasKey(x => x.Id);
+            builder.HasOne(x => x.Client).WithMany().OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
