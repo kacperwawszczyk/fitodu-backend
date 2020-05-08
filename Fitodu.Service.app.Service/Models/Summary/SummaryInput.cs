932dc486a32e221495b3f2338d191cdcc4c44c09
Patryk Janas <patrykjanas@mat.umk.pl>
2020-05-08 21:00:03 +0200

    summary input changes

--- Fitodu.Service.app.Service/Models/Summary/SummaryInput.cs
+++ Fitodu.Service.app.Service/Models/Summary/SummaryInput.cs
@@ -7,11 +7,15 @@ namespace Fitodu.Service.Models
 {
     public class SummaryInput
     {
+        [Required]
         public string IdClient { get; set; }
+        [Required]
         public decimal? Weight { get; set; }
+        [Required]
         public decimal? FatPercentage { get; set; }
-        [Required(AllowEmptyStrings = false, ErrorMessage = "RequiredError")]
+        //[Required(AllowEmptyStrings = false, ErrorMessage = "RequiredError")]
         public string Description { get; set; }
+        [Required]
         public DateTime? Date { get; set; }
     }
 }
--- Fitodu.Service.app.Service/Models/Summary/UpdateSummaryInput.cs
+++ Fitodu.Service.app.Service/Models/Summary/UpdateSummaryInput.cs
@@ -7,12 +7,17 @@ namespace Fitodu.Service.Models
 {
     public class UpdateSummaryInput
     {
+        [Required]
         public int Id { get; set; }
+        [Required]
         public string IdClient { get; set; }
+        [Required]
         public decimal? Weight { get; set; }
+        [Required]
         public decimal? FatPercentage { get; set; }
-        [Required(AllowEmptyStrings = false, ErrorMessage = "RequiredError")]
+        //[Required(AllowEmptyStrings = false, ErrorMessage = "RequiredError")]
         public string Description { get; set; }
+        [Required]
         public DateTime? Date { get; set; }
     }
 }
